b5411ce9b2c89867d845fa61d2d49962dc76f482
DogFather <33054546+DogFather@users.noreply.github.com>
2021-08-25 20:42:08 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -12,3 +12,8 @@
 
 # Modify default IP
 sed -i 's/192.168.1.1/192.168.5.1/g' package/base-files/files/bin/config_generate
+# 获取luci-app-openclash 编译po2lmo
+git clone -b master https://github.com/vernesong/OpenClash package/diy-packages/openclash
+pushd package/diy-packages/openclash/luci-app-openclash/tools/po2lmo
+make && sudo make install
+popd
